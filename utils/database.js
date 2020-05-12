40da3840d5d533b7b49cd713f6d801b448fa24e6
pierrolabo <pierrolabo@gmail.com>
2020-05-12 13:14:43 +0200

    Database connection

---
+++ utils/database.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+let _db;
+
+const mongoConnect = (callback) => {
+  mongoose
+    .connect(process.env.DATABASE_LOCALE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((client) => {
+      console.log('Mongo Connected!');
+      _db = client;
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
